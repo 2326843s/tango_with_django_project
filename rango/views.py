d9cf0ac91efb3c0641533ac0eb71d89a342fbf41
Johnathan Sciallo <2326843s@student.gla.ac.uk>
2020-02-14 13:17:59 +0000

    Chapter 8 mid exercise version

--- rango/views.py
+++ rango/views.py
@@ -9,8 +9,6 @@ from django.urls import reverse
 
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
@@ -18,31 +16,17 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = pages_list
     
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {'boldmessage': 'This tutorial has been put together by Johnathan Sciallo'}
     
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/about.html', context=context_dict)
     
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
     context_dict = {}
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-        # Retrieve all of the associated pages.
-        # The filter() will return a list of page objects or an empty list.
+        
         pages = Page.objects.filter(category=category)
         # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
